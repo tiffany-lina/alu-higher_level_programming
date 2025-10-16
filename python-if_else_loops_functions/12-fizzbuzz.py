ea37cf30884217c71e994350bd885723a2aee759
tiffany <t.turate@alustudent.com>
2025-10-16 11:27:57 -0700

    Add 12-fizzbuzz.py to print 1-100 with FizzBuzz rules

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    """Print numbers 1 to 100 with FizzBuzz rules, separated by space."""
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
+        elif i % 5 == 0:
+            print("Buzz", end=' ')
+        else:
+            print(i, end=' ')
