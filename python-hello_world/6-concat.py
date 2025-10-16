cc7a48b771f45cb13822d8266701cd7637512fbf
tiffany <t.turate@alustudent.com>
2025-10-16 09:55:50 -0700

    Update 6-concat.py to print Holberton School and Battery Street using str3

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "Welcome to "
-str2 = "Battery Street!"  # or "Holberton School!" depending on the expected output
-print(str1 + str2)
+str1="Welcome to "; str2="Holberton "; str3="School!"; print(str1+str2+str3)
+str2="Battery "; str3="Street!"; print(str1+str2+str3)
 
