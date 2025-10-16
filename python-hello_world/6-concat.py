a5ead15f6f54502c9b78441d915bef30bf6e0618
tiffany <t.turate@alustudent.com>
2025-10-16 09:58:42 -0700

    Update 6-concat.py to print Welcome to Holberton School

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str1="Welcome to "; str2="Holberton "; str3="School!"; print(str1+str2+str3)
-str2="Battery "; str3="Street!"; print(str1+str2+str3)
+str1 = "Welcome to "
+str2 = "Holberton School!"
+print(str1 + str2)
 
