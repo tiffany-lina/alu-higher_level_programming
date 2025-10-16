18f8e5009c0b9b97a6d5aa8b98989a6f8e1b07a1
tiffany <t.turate@alustudent.com>
2025-10-16 11:14:39 -0700

    Fix 8-uppercase.py to handle all edge cases and PEP8 compliant

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def uppercase(str):
     """Print a string in uppercase using only one loop and no imports."""
     for c in str:
         print("{}".format(chr(ord(c) - 32)) if 'a' <= c <= 'z' else "{}".format(c),
               end='')
     print()
-
