52a7d6a58b1030c0ca11b929c1acb308a431ea63
tiffany <t.turate@alustudent.com>
2025-11-14 10:26:56 -0800

    Add write_file function, main script, and update README for task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that writes a string to a UTF-8 text file
+and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes `text` to `filename` (UTF-8) and returns number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
--- python-input_output/README.md
+++ python-input_output/README.md
@@ -6,3 +6,8 @@ Write a function that reads a UTF-8 text file and prints it to stdout.
 - Must use the `with` statement
 - No imports needed
 - No exception handling required
+## 1. Write to a file
+
+Write a function that writes a string to a UTF-8 text file and returns
+the number of characters written.
+
