6805d5311045f7f647c0b62c4c70d9cee30d9dc2
tiffany <t.turate@alustudent.com>
2025-11-14 07:59:12 -0800

    Cleaned BaseGeometry class; fixed PEP8 and line endings

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""Empty BaseGeometry class"""
+"""Empty BaseGeometry class."""
 
 
 class BaseGeometry:
-    """Class Geometry"""
+    """BaseGeometry class."""
 
     def area(self):
-        """Raises Exception only"""
-        raise Exception('area() is not implemented')
+        """Raises an Exception since area() is not implemented."""
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Integer validator if less than 0 or not int"""
-        if type(value) != int:
+        """Validate that `value` is a positive integer.
+
+        Args:
+            name (str): The name of the parameter.
+            value: The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value <= 0.
+        """
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
