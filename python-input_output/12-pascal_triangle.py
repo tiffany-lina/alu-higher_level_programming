ef1cc5c2b82dfeae1551c7c01b7668d424697643
tiffany <t.turate@alustudent.com>
2025-11-14 11:23:52 -0800

    Add pascal_triangle function for Pascal's triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+12-pascal_triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[i - 1]
+        row = [1]
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+        row.append(1)
+        triangle.append(row)
+
+    return triangle
