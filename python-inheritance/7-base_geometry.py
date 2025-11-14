c2318115faddcf58f29e7db9d765eb4d5bc75600
tiffany <t.turate@alustudent.com>
2025-11-14 09:14:43 -0800

    Add BaseGeometry class with area and integer_validator, ALU compliant

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""Empty BaseGeometry class."""
-
+"""Module 7-base_geometry: Defines BaseGeometry class with validation."""
 
 class BaseGeometry:
-    """BaseGeometry class."""
+    """BaseGeometry class that defines geometric operations."""
 
     def area(self):
-        """Raises an Exception since area() is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that `value` is a positive integer.
-
-        Args:
-            name (str): The name of the parameter.
-            value: The value to validate.
+        """Calculate area.
 
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value <= 0.
+            Exception: Always raises 'area() is not implemented'
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validate that a value is a positive integer.
+
+        Args:
+            name (str): The name of the value
+            value (int): The value to validate
+
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is <= 0
         """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
