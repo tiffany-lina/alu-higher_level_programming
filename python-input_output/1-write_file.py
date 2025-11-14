b8aec371027bf9396def3a5acc5574c5be84e9cb
tiffany <t.turate@alustudent.com>
2025-11-14 10:28:53 -0800

    Add write_file function, main script, and update README for task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,9 @@ and returns the number of characters written.
 
 
 def write_file(filename="", text=""):
-    """Writes `text` to `filename` (UTF-8) and returns number of characters written"""
+    """
+    Writes `text` to `filename` (UTF-8) and returns
+    the number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
