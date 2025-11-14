3d7dabe8a49b5e5135310db17e2608cedfd4632b
tiffany <t.turate@alustudent.com>
2025-11-14 07:08:05 -0800

    Add is_same_class function to check exact class of an object

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Module that defines a function to check exact class of an object."""
+"""Module that defines a function to check the exact class
+of an object."""
 
 
 def is_same_class(obj, a_class):
-    """Return True if obj is exactly an instance of a_class, otherwise False."""
+    """Return True if obj is exactly an instance of a_class.
+    Otherwise False."""
     return type(obj) is a_class
