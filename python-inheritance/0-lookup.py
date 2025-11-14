15033b16ff09c5ecd2cbfdde351ead873e015d42
tiffany <t.turate@alustudent.com>
2025-11-14 06:32:37 -0800

    Add lookup function and update README

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines a function to list available attributes and methods of an object"""
+"""
+Module that defines a function to list available attributes
+and methods of an object.
+"""
 
 
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object"""
+    """Return a list of available attributes and methods of an object."""
     return dir(obj)
