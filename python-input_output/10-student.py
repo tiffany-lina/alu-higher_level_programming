81a9915e5a50d2ee84c9f55c35d18ae5d70c37f5
tiffany <t.turate@alustudent.com>
2025-11-14 10:51:22 -0800

    Add Student class with selective to_json method and main script for task 10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -26,5 +26,9 @@ class Student:
         Otherwise, all attributes are included.
         """
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
-            return {k: v for k, v in self.__dict__.items() if k in attrs}
+            return {
+                k: v
+                for k, v in self.__dict__.items()
+                if k in attrs
+            }
         return self.__dict__.copy()
