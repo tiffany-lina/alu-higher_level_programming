e5e62869f3111459a0fda003657098b844b86f6a
tiffany <t.turate@alustudent.com>
2025-11-14 10:20:33 -0800

    Add read_file function and main script

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Function that reads a text file (UTF8) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """Reads a UTF-8 text file and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
